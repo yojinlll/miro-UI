a21ff61a8080b43c7fcce79e0df4fb8bd31f98ea
yojinlll <yojin1995@qq.com>
2019-01-23 13:56:37 +0800

    添加四个 chai.assert 简单用例

--- src/app.js
+++ src/app.js
@@ -17,3 +17,91 @@ new Vue({
     }
   }
 })
+
+
+import chai from 'chai'
+
+const expect = chai.expect
+const assert = chai.assert
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  button.$mount()
+  let use = button.$el.querySelector('use')
+  let icon = use.getAttribute('xlink:href')
+  assert.equal(icon, '#i-settings')
+  button.$el.remove()
+  button.$destroy
+}
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.toggle = true
+  button.$mount()
+  let use = button.$el.querySelector('use')
+  let icon = use.getAttribute('xlink:href')
+  assert.equal(icon, '#i-loading')
+  button.$el.remove()
+  button.$destroy
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  assert.equal(order, '1')
+  button.$el.remove()
+  button.$destroy
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  assert.equal(order, '2')
+  button.$el.remove()
+  button.$destroy
+}
+{
+  // const Constructor = Vue.extend(Button)
+  // const clickButton = new Constructor({
+  //   propsData: {
+  //     icon: 'settings',
+  //     loading: true
+  //   }
+  // })
+  // clickButton.$mount()
+  // clickButton.onToggle()
+  // clickButton.$nextTick(() => {
+  //     let use = clickButton.$el.querySelector('use')
+  //     let icon = use.getAttribute('xlink:href')
+  //     assert.equal(icon,'#i-loading')
+  //   }
+  // )
+  // clickButton.$el.remove()
+  // clickButton.$destroy
+}
