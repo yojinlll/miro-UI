7594c0a1f90d293a5a50efb26c6502a4f0062da2
yojinlll <yojin1995@qq.com>
2019-01-26 02:29:41 +0800

    给 row & col 组件添加响应式样式

--- src/miro-col.vue
+++ src/miro-col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="miro-col" :class="[span && `col-${span}`, offset && `offset-${offset}`, gutter && `gutter-${gutter}`]">
+  <div class="miro-col" :class="colClass">
     <slot></slot>
   </div>
 </template>
@@ -9,22 +9,63 @@
     name: "miroCol",
     props: {
       span: {type: [Number, String]},
-      offset: {type: [Number, String]}
+      offset: {type: [Number, String]},
+      phone: {type: [Number, String, Object]},
+      ipad: {type: [Number, String, Object]}
     },
     computed: {
-      gutter(){return this.$parent.gutter}
-    },
+      gutter(){
+        return this.$parent.gutter
+      },
+      colClass(){
+        let classArray = []
+        let {span, offset, gutter, phone, ipad} = this
 
+        classArray = [
+          gutter && `gutter-${gutter}`,
+          span && `span-${span}`,
+          offset && `offset-${offset}`,
+        ]
+
+        if (typeof phone === 'object') {
+          classArray.push(
+            phone.span && `phone-span-${phone.span}`,
+            phone.offset && `phone-offset-${phone.offset}`
+          )
+        }else {
+          classArray.push(
+            phone && `phone-span-${phone}`
+          )
+        }
+
+        if (typeof ipad === 'object') {
+          classArray.push(
+            ipad.span && `ipad-span-${ipad.span}`,
+            ipad.offset && `ipad-offset-${ipad.offset}`
+          )
+        }else {
+          classArray.push(
+            ipad && `ipad-span-${ipad}`
+          )
+        }
+
+        return classArray
+      },
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   .miro-col {
-    height: 50px;
-    background: lightblue;
     width: 100%;
-    border: 1px solid #6c6c6c;
-    $class-prefix: col-;
+    $class-prefix: gutter-;
+    @for $n from 1 through 100 {
+      &.#{$class-prefix}#{$n}:not(:last-child) {
+        margin-right: $n * 1px;
+      }
+    }
+
+    $class-prefix: span-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
@@ -36,11 +77,36 @@
         margin-left: ($n / 24) * 100%;
       }
     }
-    $class-prefix: gutter-;
-    @for $n from 1 through 100 {
-      &.#{$class-prefix}#{$n}:not(:first-child) {
-        margin-left: $n * 1px;
+
+    @media (min-width: 577px) and (max-width: 993px) {
+      $class-prefix: ipad-span-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: ipad-offset-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
+
+    @media (max-width: 577px) {
+      $class-prefix: phone-span-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: phone-offset-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
   }
 </style>
