9c963bcc05b59c34fec906239c55fb0ca7de6abc
yojinlll <yojin1995@qq.com>
2019-02-07 17:26:41 +0800

    input 组件添加 placeholder 选项

--- src/miro-input.vue
+++ src/miro-input.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="miro-input" :class="{error}">
     <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
+           :placeholder="placeholder"
            @change="$emit('change', $event.target.value)"
            @input="$emit('input', $event.target.value)"
            @focus="$emit('focus', $event.target.value)"
@@ -20,6 +21,10 @@
     name: "miroInput",
     props: {
       value: {
+        type: String,
+        default: ''
+      },
+      placeholder: {
         type: String,
         default: ''
       },
